31a165c4b4bc3c108407f7dff4261744bb896c37
Julia Lepiscopo <julia.lepiscopo@gmail.com>
2026-09-17 11:54:02 -0300

    Alteração da tipagem da propriedade Autor

--- src/models/Livro.js
+++ src/models/Livro.js
@@ -8,7 +8,11 @@ const livroSchema = new mongoose.Schema({
     editora: { type: String },
     preco: { type: Number },
     paginas: { type: Number },
-    autor: autorSchema
+    autor: { 
+        type: mongoose.Schema.Types.ObjectId, 
+        ref: "autores", 
+        required: true 
+    }
 }, { versionKey: false });
 
 const livro = mongoose.model("livros", livroSchema);
