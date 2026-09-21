3b01029b6730db541abb7293fdec5cd239a36030
Julia Lepiscopo <julia.lepiscopo@gmail.com>
2026-09-21 11:33:47 -0300

    [API][Task] - Refs #27 - Criar classes RequisicaoIncorreta

---
+++ src/erros/RequisicaoIncorreta.js
@@ -0,0 +1,8 @@
+import ErroBase from "../erros/ErroBase.js";
+
+class RequisicaoIncorreta extends ErroBase {
+  constructor(mensagem = "Um ou mais dados fornecidos estão incorretos") {
+    super(mensagem, 400);
+   } }
+
+   export default RequisicaoIncorreta;
