65f435e4478e09072a0155011866ad7e9f4191c2
Julia Lepiscopo <julia.lepiscopo@gmail.com>
2026-09-21 09:59:46 -0300

    [API][Task] - Closes #27 - Tratar erros de validação e personalizar mensagens nos modelos Mongoose

--- src/middlewares/manipuladorDeErros.js
+++ src/middlewares/manipuladorDeErros.js
@@ -4,6 +4,11 @@ import mongoose from "mongoose";
 const manipuladorDeErros = (erro, req, res, next) => {
     if (erro instanceof mongoose.Error.CastError) {
         res.status(400).send({ message: "Id inválido." });
+    }else if (erro instanceof mongoose.Error.ValidationError) {
+        const mensagensDeErro = Object.values(erro.errors)
+        .map(erro => erro.message)
+        .join(", ");
+        res.status(400).send({ message: `Os seguintes erros foram encontrados: ${mensagensDeErro}` });
     } else {
         res.status(500).send({ message: "Erro interno no servidor" });
     } 
--- src/models/Autor.js
+++ src/models/Autor.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 // Define o schema do autor
 const autorSchema = new mongoose.Schema({
     id: { type: mongoose.Schema.Types.ObjectId },
-    nome: { type: String, required: [true, "O nome do autor é obrigatório"], trim: true },
+    nome: { type: String, required: [true, "O nome do(a) autor(a) é obrigatório"], trim: true },
     nacionalidade: { type: String, trim: true }
 }, { versionKey: false });
 
