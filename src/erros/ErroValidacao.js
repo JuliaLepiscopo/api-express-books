8823d31ee8a1e31e3910ba1445c95fafd2976812
Julia Lepiscopo <julia.lepiscopo@gmail.com>
2026-09-21 11:34:05 -0300

    [API][Task] - Refs #27 - Criar classe ErroValidacao

---
+++ src/erros/ErroValidacao.js
@@ -0,0 +1,11 @@
+import RequisicaoIncorreta from "../erros/RequisicaoIncorreta.js";
+
+class ErroValidacao extends RequisicaoIncorreta {
+  constructor(erro) {
+    const mensagensDeErro = Object.values(erro.errors)
+        .map((erro) => erro.message)
+        .join(", ");
+    super(`Os seguintes erros foram encontrados: ${mensagensDeErro}`);
+   } }
+
+   export default ErroValidacao;
