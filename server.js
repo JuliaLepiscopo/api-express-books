1434f51df5a921f17e787ab25862b5d7782cc9f5
Julia Lepiscopo <julia.lepiscopo@gmail.com>
2026-09-21 08:28:55 -0300

    [API] - Closes #20 - Adicionar tratamento de erro 404 e next(erro) no AutorController"

--- server.js
+++ server.js
@@ -5,18 +5,5 @@ import app from "./src/app.js";
 
 const PORT = 3000;
 
-const rotas = {
-    "/": "Livros API",
-    "/contato": "Contato",
-    "/sobre": "Sobre",
-    "/livros": "Lista de livros"
-};
-
-//const server = http.createServer((req, res) => {
-  //  const rota = rotas[req.url] || "Rota não encontrada";
-    //res.writeHead(200, { "Content-Type": "text/plain" });
-    //res.end(rotas[req.url]);
-//});
-
 app.listen(PORT, () => { console.log("Servidor escutando"); });
 
--- src/controllers/autorControllers.js
+++ src/controllers/autorControllers.js
@@ -1,60 +1,75 @@
-import { autor } from "../models/Autor.js";
+import { autor as Autor } from "../models/Autor.js";
 
 class AutorController {
-    // Busca todos os autores no MongoDB
-    static async listarAutores (req, res){
-        try{
-        const listaAutores = await autor.find({});
-        res.status(200).json(listaAutores);
-    } catch (erro) {
-        res.status(500).json({ mensagem: `${erro.message} - falha na requisição dos livros` });
-    }}
-
-
-    // Busca um autor específico pelo ID do MongoDB
-    static async listarAutorPorId (req, res){
-        try {
-            const id = req.params.id;
-        const autorEncontrado = await autor.findById(id);
-        res.status(200).json(autorEncontrado);
-    } catch (erro) {
-        res.status(500).json({ mensagem: `${erro.message} - falha na requisição do autor` });
-    }
-    }
-
-
-    // Cadastra um novo autor no MongoDB
-    static async cadastrarAutor (req, res){
-        try {
-            const novoAutor = await autor.create(req.body);
-            res.status(201).json({ mensagem: "Autor adicionado com sucesso!", autor: novoAutor });
-        } catch (erro) {
-            res.status(500).json({ mensagem: `${erro.message} - falha ao cadastrar autor` });
-        }
-    }
-
-    // Atualiza um autor existente no MongoDB
-    static async atualizarAutor (req, res){
+  // Busca todos os autores no MongoDB
+  static listarAutores = async (req, res) => {
     try {
-        const id = req.params.id;
-        await autor.findByIdAndUpdate(id, req.body);
-        res.status(200).json({ mensagem: "Autor atualizado com sucesso" });
-    } catch (erro) {
-        res.status(500).json({ mensagem: `${erro.message} - falha na atualização` });
-    }
-};
+      const autoresResultado = await Autor.find();
 
-    // Deleta um autor existente no MongoDB
-    static async excluirAutor (req, res){
+      res.status(200).json(autoresResultado);
+    } catch (erro) {
+      console.error(erro);
+      res.status(500).json({ message: "Erro interno no servidor" });
+    }
+  };
+
+  // Busca um autor específico pelo ID do MongoDB
+  static listarAutorPorId = async (req, res, next) => {
     try {
-        const id = req.params.id;
-        await autor.findByIdAndDelete(id);
-        res.status(200).json({ mensagem: "Autor deletado com sucesso" });
-    } catch (erro) {
-        res.status(500).json({ mensagem: `${erro.message} - falha na exclusão` });
-    }
-};
+      const id = req.params.id;
 
-};
+      const autorResultado = await Autor.findById(id);
+      
+      // Tratamento para retornar 404 caso o ID não exista
+      if (autorResultado !== null) {
+        res.status(200).send(autorResultado);
+      } else {
+        res.status(404).send({ message: "Id do Autor não localizado." });
+      }
+    } catch (erro) {
+      next(erro); // Passa o erro para o middleware de tratamento de erros
+    } // <--- A CHAVE QUE FALTAVA FOI ADICIONADA AQUI PARA FECHAR O CATCH
+  }; 
+
+  // Cadastra um novo autor no MongoDB
+  static cadastrarAutor = async (req, res) => {
+    try {
+      let autor = new Autor(req.body);
+
+      const autorResultado = await autor.save();
+
+      res.status(201).send(autorResultado.toJSON());
+    } catch (erro) {
+      console.error(erro);
+      res.status(500).send({ message: "Falha ao cadastrar Autor." });
+    }
+  };
+
+  // Atualiza um autor existente no MongoDB
+  static atualizarAutor = async (req, res) => {
+    try {
+      const id = req.params.id;
+  
+      await Autor.findByIdAndUpdate(id, { $set: req.body });
+
+      res.status(200).send({ message: "Autor atualizado com sucesso" });
+    } catch (erro) {
+      res.status(500).send({ message: erro.message });
+    }
+  };
+
+  // Deleta um autor existente no MongoDB
+  static excluirAutor = async (req, res) => {
+    try {
+      const id = req.params.id;
+
+      await Autor.findByIdAndDelete(id);
+
+      res.status(200).send({ message: "Autor removido com sucesso" });
+    } catch (erro) {
+      res.status(500).send({ message: erro.message });
+    }
+  };
+}
 
 export default AutorController;
