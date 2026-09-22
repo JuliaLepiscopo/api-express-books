12e59f76420deeab8001234f74cbe43b2aaca6a6
Julia Lepiscopo <julia.lepiscopo@gmail.com>
2026-09-22 09:18:33 -0300

    [API][Task] - Closes #40 -Validações Mongoose

--- src/models/Livro.js
+++ src/models/Livro.js
@@ -3,10 +3,22 @@ import mongoose from "mongoose";
 // Define o schema do livro
 const livroSchema = new mongoose.Schema({
     id: { type: mongoose.Schema.Types.ObjectId },
-    titulo: { type: String, required: [true, "O título do livro é obrigatório"], trim: true },
-    editora: { type: String, required: [true, "A editora do livro é obrigatória"], trim: true },
+    titulo: { 
+        type: String, 
+        required: [true, "O título do livro é obrigatório"], 
+        trim: true },
+    editora: { 
+        type: String, 
+        required: [true, "A editora do livro é obrigatória"], 
+        trim: true, 
+        enum: {
+            values: ["Editora Brasil", "Classico"],
+            message: "Não foi possível reconhecer o nome da editora {VALUE}." }},
     preco: { type: Number },
-    paginas: { type: Number },
+    numeroPaginas: { 
+        type: Number, 
+        min:[10, "O número de páginas deve ser acima de 10. Valor fornecido: {VALUE}"], 
+        max: [5000, "O número de páginas não pode passar de 5000. Valor fornecido: {VALUE}"] },
     autor: { 
         type: mongoose.Schema.Types.ObjectId, 
         ref: "autores", 
